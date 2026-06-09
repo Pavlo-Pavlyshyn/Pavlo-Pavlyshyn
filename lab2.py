9e04d2f1f1ae99562bc9f33048faa088213ab67b
Pavlo-Pavlyshyn <pavlyshyn.pavlentiy@gmail.com>
2026-06-09 18:13:50 +0300

    lab2.py

---
+++ lab2.py
@@ -0,0 +1,28 @@
+# x = object()
+# y = object()
+
+# # TODO: change this code
+# x_list = [x]
+# y_list = [y]
+# big_list = []
+
+# big_list = x_list*10 + y_list*10
+
+
+
+numbers = []
+strings = []
+names = ["John", "Eric", "Jessica"]
+
+# write your code here
+second_name = None
+second_name = names[1]
+
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)  
+strings.append("hello") 
+strings.append("world")
+
+print(numbers)
+print(strings)
